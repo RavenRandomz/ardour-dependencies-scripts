85ddc4f7a1e568cb247088bd8e5bb2cfe4ad8283
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-03 21:20:23 -0500

    feat(ubuntu): Add liblilv

--- ardour-dependencies-ubuntu.sh
+++ ardour-dependencies-ubuntu.sh
@@ -3,6 +3,7 @@ sudo apt install libboost-all-dev build-essential python3 python-is-python3 liba
 	libtag1-dev vamp-plugin-sdk librubberband-dev libusb-1.0-0-dev\
 	libjack-jackd2-dev clang libclang-cpp-dev libfftw3-dev libudev-dev libaubio-dev libpng++-dev\
 	libpango1.0-dev libxrandr-dev libcairomm-1.0-dev libpangomm-1.4-dev libxinerama-dev liblv2dynparam1-dev\
-	libcppunit-dev libreadline-dev libwebsockets-dev libcwiid-dev liblrdf0-dev 
+	libcppunit-dev libreadline-dev libwebsockets-dev libcwiid-dev liblrdf0-dev liblilv-dev
 
+#Liblilv on Ubuntu Plucky Puffin is now past the errant versions between 0.24.20 until 0.24.23 (fixed in 0.24.23)
 #make sure liblilv i installed, it cant be manually installed from the ardour dependency chain it requires the version to be 24
